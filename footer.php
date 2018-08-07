819d6a21d417e0fbfe2166ee4e31f549b52aab87
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-08-08 00:00:01 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <?php
-echo 'Designed & Developed by <a class="white-text text-lighten-3" href="http://ashishpatelmedia.com">Ashish Patel</a> under the guidance of <a class="white-text text-lighten-3" href="http://sumandeb.com">Sir Suman Deb</a> at HCI AI Laboratory, National Institute of Technology, Agartala. <br> &copy; <span id="copyear">2017</span>. All Rights Reserved.';
+echo 'Designed & Developed by <a class="white-text text-lighten-3" href="http://www.ashishpatel.us">Ashish Patel</a> under the guidance of <a class="white-text text-lighten-3" href="http://sumandeb.com">Sir Suman Deb</a> at HCI AI Laboratory, National Institute of Technology, Agartala. <br> &copy; <span id="copyear">2017</span>. All Rights Reserved.';
 ?>
 
 <script>
